ce44494e25e691f7dda608820758f2617f861abb
Ethan Wang <ethan6688@gmail.com>
2024-02-02 16:46:58 +1100

    Oops Input was needed

---
+++ frontend/src/components/Input/inputStyle.ts
@@ -0,0 +1,22 @@
+import { styled } from 'goober'
+import { forwardRef } from 'react'
+
+export const StyledInput = styled('input', forwardRef)<{as: string}>`
+  font: inherit;
+  border: 1px solid var(--border-color, var(--input-border));
+  background: var(--white);
+  border-radius: .3em;
+  padding: .6em .9em;
+  width: 100%;
+  box-sizing: border-box;
+  ${props => props.as === 'textarea' && 'resize: vertical;'}
+  transition: box-shadow .1s, border-color .1s;
+  color: var(--black);
+  margin: 0;
+
+  &:focus {
+    outline: 0;
+    box-shadow: inset 0 0 0 1px var(--border-color, var(--primary));
+    border-color: var(--border-color, var(--primary));
+  }
+`
