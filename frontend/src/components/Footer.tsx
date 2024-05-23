eec72546c26b5692015b3ed5724aaf75e4784fc1
Ethan Wang <ethan6688@gmail.com>
2024-05-23 17:50:57 +1000

    tailwind scans code to generate classes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,20 +1,22 @@
-import { AppColors } from '../config/style'
+import { AppColor } from '../config/style'
 
 const FooterItem = ({ children }: { children: React.ReactNode }) => (
-  <span className={`text-sm ${AppColors.textBlack}`}>{children}</span>
+  <span
+    className={`text-sm ${AppColor.blackText} [&_a]:underline [&_a]:text-inherit ${AppColor.primaryAChildHover}`}>
+    {children}
+  </span>
 )
 
 const Footer = () => (
   <footer
-    className={`flex items-center gap-4 p-4 mt-auto bottom-0 left-0 w-full min-h-12 box-border ${AppColors.bgToolbar}`}>
+    className={`flex items-center gap-4 p-4 mt-auto bottom-0 left-0 w-full min-h-12 box-border ${AppColor.toolbarBg}`}>
     <FooterItem>anioncat 2024.</FooterItem>
     <div className=" flex-1" />
     <FooterItem>
       <a
         href="https://github.com/anioncat/habit-tracker"
         target="_blank"
-        rel="noreferrer nofollow"
-        className={`underline text-inherit ${AppColors.hoverTextPrimary}`}>
+        rel="noreferrer nofollow">
         Source Code
       </a>
     </FooterItem>
--- frontend/src/config/style.ts
+++ frontend/src/config/style.ts
@@ -1,7 +1,12 @@
-export const AppColors = {
-  textBlack: 'text-slate-950',
-  bgToolbar: 'bg-slate-100',
-  textPrimary: 'text-violet-800',
-  bgPrimary: 'bg-violet-800',
-  hoverTextPrimary: 'hover:text-violet-800',
+export const AppColor = {
+  blackText: 'text-slate-950',
+  blackBg: 'bg-slate-950',
+
+  toolbarText: 'text-slate-100',
+  toolbarBg: 'bg-slate-100',
+
+  primaryText: 'text-violet-800',
+  primaryBg: 'bg-violet-800',
+  primaryAChildHover: '[&_a]:hover:text-violet-800',
+  primaryHoverText: 'hover:text-violet-800',
 }
