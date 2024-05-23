713282815ccb980e10171888718d0d30cbbba07d
Ethan Wang <ethan6688@gmail.com>
2024-05-23 18:00:38 +1000

    Use selectors for parent components

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = ({ center, linkTo }: { center?: boolean; linkTo?: To }) => {
   const content = (
     <>
       {/* <Logo /> */}
-      <h1 className="font-bold text-2xl">Mood Tracker</h1>
+      <h1>Mood Tracker</h1>
     </>
   )
 
@@ -14,7 +14,8 @@ const Header = ({ center, linkTo }: { center?: boolean; linkTo?: To }) => {
   const justify = center ? 'justify-center' : ''
 
   return (
-    <header className={`${justify} flex items-center mb-4 gap-4 select-none`}>
+    <header
+      className={`${justify} flex items-center mb-4 gap-4 select-none [&_a]:text-inherit [&_a]:no-underline [&_a]:flex [&_a]:items-center [&_a]:gap-4 [&_h1]:font-bold [&_h1]:text-2xl [&_h1]:pr-2 [&_h1]:mb-2`}>
       <HeaderContent></HeaderContent>
     </header>
   )
--- frontend/src/components/Main.tsx
+++ frontend/src/components/Main.tsx
@@ -8,7 +8,7 @@ const Main = ({ wide, ...props }: MainProps) => {
   const mWidth = wide ? 'max-w-screen-xl' : 'max-w-screen-sm'
   return (
     <main
-      className={`${mWidth} w-full py-4 px-1 box-border my-0 mx-auto pb-12`}
+      className={`${mWidth} w-full py-4 px-1 box-border my-0 mx-auto pb-12 min-w-[360px]`}
       {...props}>
       {props.children}
     </main>
