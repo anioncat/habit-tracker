bb242acec2222618756575a7b5b094fd4939f4de
Ethan Wang <ethan6688@gmail.com>
2024-06-19 17:59:35 +1000

    Move bundling to outside script

--- BUILD_ALL.sh
+++ BUILD_ALL.sh
@@ -7,8 +7,13 @@ cd $FRONTEND_DIR
 echo entered $PWD
 echo
 bun run build
+echo Creating release tarball...
+mkdir -p ../release
+rm -f ../release/frontend-release.tar.gz
+tar -czf ../release/frontend-release.tar.gz dist
 echo
+
 cd ../$BACKEND_DIR
 echo entered $PWD
 echo
-make
+make zip
